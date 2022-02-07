02dce1c3a1c76a9e52ace9adf36d7097d7bf6008
Bartłomiej Koczenasz <malgo.skowiera@gmail.com>
2022-02-07 17:26:58 +0100

    zadania z React Lekcja 2 do 4

--- my-app/src/components/ClassCounterBoom.jsx
+++ my-app/src/components/ClassCounterBoom.jsx
@@ -5,29 +5,27 @@ export default class ClassCounterBoom extends Component {
     super(props);
     this.state = { counting: 5 };
 
-    let counting = this.state;
-    function countingDown() {
-      if (counting > 1) {
-        while (counting > 1) {
-          setTimeout(
-            () =>
-              this.setState((state) => ({ ...state, counting: counting - 1 })),
-            1000
-          );
-          counting = counting - 1;
-        }
-      } else {
-        counting = "Boom!lekcja 4 zad 6";
-        this.setState({ counting });
-      }
-    }
+    let { counting } = this.state;
+    setTimeout(() => {
+      while (counting > 0) counting = counting - 1;
+
+      this.setState({ counting });
+    }, 5000);
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.state.counting}</h1>
-      </div>
-    );
+    if (this.state.counting > 0) {
+      return (
+        <div>
+          <h1>{this.state.counting}</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Boom!Lekcja 4 zadanie 6</h1>
+        </div>
+      );
+    }
   }
 }
